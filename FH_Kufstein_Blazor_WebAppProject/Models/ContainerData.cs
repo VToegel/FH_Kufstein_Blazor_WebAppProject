2f3f9a911b8452837888af9e6793d82fd5558772
Vincent Tögel <vincent.toegel@googlemail.com>
2024-05-01 16:39:10 +0200

    Projektdateien hinzufügen.

---
+++ FH_Kufstein_Blazor_WebAppProject/Models/ContainerData.cs
@@ -0,0 +1,14 @@
+﻿namespace FH_Kufstein_Blazor_WebAppProject.Models
+{
+    public class ContainerData
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public required string Location { get; set; }
+        public required string ContainerType { get; set; }
+        public int Capacity { get; set; }
+        public int CurrentLevel { get; set; }
+        public string[]? Measurements { get; set; }
+    }
+}
---
+++ FH_Kufstein_Blazor_WebAppProject/Program.cs
@@ -0,0 +1,25 @@
+using FH_Kufstein_Blazor_WebAppProject.Components;
+
+var builder = WebApplication.CreateBuilder(args); // Create a new WebApplication instance & create builder to register for dependency injection
+
+// Add services to the container.
+builder.Services.AddRazorComponents(); // Add RazorComponents to the services container
+
+var app = builder.Build(); // Build the WebApplication
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts(); //Enforce HTTPS
+}
+
+app.UseHttpsRedirection(); //Redirect HTTP to HTTPS
+
+app.UseStaticFiles(); // Serve static files, e.g. HTML, CSS, JS
+app.UseAntiforgery(); // Add antiforgery token support to prevent cross-site request forgery attacks (CSRF)
+
+app.MapRazorComponents<App>(); // Map the RazorComponents to the App component
+
+app.Run();
