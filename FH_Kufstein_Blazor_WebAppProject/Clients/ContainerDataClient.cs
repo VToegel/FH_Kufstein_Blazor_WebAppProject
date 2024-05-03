80a7bbdf105bcfb68ebf0b31897d2d6012f91870
Vincent Tögel <vincent.toegel@googlemail.com>
2024-05-03 20:27:23 +0200

    finalized exercise

--- FH_Kufstein_Blazor_WebAppProject/Clients/ContainerDataClient.cs
+++ FH_Kufstein_Blazor_WebAppProject/Clients/ContainerDataClient.cs
@@ -6,19 +6,18 @@ namespace FH_Kufstein_Blazor_WebAppProject.Clients
     {
         private readonly List<ContainerData> containers =
         [
-        new ContainerData { Id = 1, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Barrel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
-        new ContainerData { Id = 2, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Barrel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
-        new ContainerData { Id = 3, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Barrel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
-        new ContainerData { Id = 4, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Barrel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
-        new ContainerData { Id = 5, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Barrel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] }
+        new ContainerData { Id = 1, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Castor", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
+        new ContainerData { Id = 2, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Castor", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
+        new ContainerData { Id = 3, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Lead Lined", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
+        new ContainerData { Id = 4, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Stainless Steel", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] },
+        new ContainerData { Id = 5, Name = $"Container {new Random().Next(0,200)}", DateTime = DateTime.Now, Location = "Gorleben", ContainerType = "Lead lined Composite", Capacity = 5000, CurrentLevel = new Random().Next(1000,5000), Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0,50)}C, RadioActivity: {new Random().Next(200,1000)}mSV"] }
         ];
 
         public ContainerData[] GetContainerData() => [.. containers];
         public ContainerType[] GetContainerTypes() => new ContainerTypeClient().GetContainerTypes();
         public void AddContainerData(ContainerDetails containerDetails)
         {
-            ArgumentException.ThrowIfNullOrWhiteSpace(containerDetails.ContainerTypeId);
-            ContainerType containerType = GetContainerTypes().Single(ct => ct.Id == int.Parse(containerDetails.ContainerTypeId));
+            ContainerType containerType = GetContainerTypeById(containerDetails.ContainerTypeId);
 
             containers.Add(new ContainerData
             {
@@ -31,6 +30,58 @@ namespace FH_Kufstein_Blazor_WebAppProject.Clients
                 CurrentLevel = containerDetails.CurrentLevel,
                 Measurements = [$"{DateTime.UtcNow}, Temperature: {new Random().Next(0, 50)}C, RadioActivity: {new Random().Next(200, 1000)}mSV"]
             });
+        }
+
+
+        public ContainerDetails GetContainerData(int id)
+        {
+            ContainerData container = GetContainerById(id);
+
+            ContainerType containerType = GetContainerTypes().Single(ct => string.Equals(ct.Name, container.ContainerType, StringComparison.OrdinalIgnoreCase));
+
+            return new ContainerDetails
+            {
+                Id = container.Id,
+                Name = container.Name,
+                DateTime = container.DateTime,
+                Location = container.Location,
+                ContainerTypeId = containerType.Id.ToString(),
+                Capacity = container.Capacity,
+                CurrentLevel = container.CurrentLevel,
+                Measurements = container.Measurements
+            };
+        }
+
+        public void UpdateContainerData(ContainerDetails updatedContainer)
+        {
+            ContainerData existingContainer = GetContainerById(updatedContainer.Id);
+
+            ContainerType containerType = GetContainerTypeById(updatedContainer.ContainerTypeId);
+
+            existingContainer.Name = updatedContainer.Name;
+            existingContainer.Location = updatedContainer.Location;
+            existingContainer.ContainerType = containerType.Name;
+            existingContainer.Capacity = updatedContainer.Capacity;
+            existingContainer.CurrentLevel = updatedContainer.CurrentLevel;
+        }
+
+        public void DeleteContainerData(int id)
+        {
+            ContainerData container = GetContainerById(id);
+            containers.Remove(container);
+        }
+
+        private ContainerData GetContainerById(int id)
+        {
+            ContainerData? container = containers.Find(c => c.Id == id);
+            ArgumentNullException.ThrowIfNull(container);
+            return container;
+        }
+
+        private ContainerType GetContainerTypeById(string? id)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(id);
+            return GetContainerTypes().Single(ct => ct.Id == int.Parse(id));
         }
     }
 }
--- FH_Kufstein_Blazor_WebAppProject/Program.cs
+++ FH_Kufstein_Blazor_WebAppProject/Program.cs
@@ -4,7 +4,7 @@ using FH_Kufstein_Blazor_WebAppProject.Components;
 var builder = WebApplication.CreateBuilder(args); // Create a new WebApplication instance & create builder to register for dependency injection
 
 // Add services to the container.
-builder.Services.AddRazorComponents(); // Add RazorComponents to the services container
+builder.Services.AddRazorComponents().AddInteractiveServerComponents(); // Add RazorComponents to the services container and add interactive server components
 builder.Services.AddSingleton<ContainerDataClient>(); // Add the ContainerDataClient to the services container
 builder.Services.AddSingleton<ContainerTypeClient>(); // Add the ContainerTypeClient to the services container
 
@@ -23,6 +23,6 @@ app.UseHttpsRedirection(); //Redirect HTTP to HTTPS
 app.UseStaticFiles(); // Serve static files, e.g. HTML, CSS, JS
 app.UseAntiforgery(); // Add antiforgery token support to prevent cross-site request forgery attacks (CSRF)
 
-app.MapRazorComponents<App>(); // Map the RazorComponents to the App component
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode(); // Map the RazorComponents to the App component and add interactive server render mode
 
 app.Run();
